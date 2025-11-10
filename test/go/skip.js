f7565a8d081399144a73d0054e888f175b9387be
dev-embedthis <mob-pub-18@emobrien.com>
2025-11-10 17:04:30 +1100

    TEST: skip go test on windows

--- test/go/skip.js
+++ test/go/skip.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env bun
+
+// Skip Go tests on Windows
+if (process.platform === 'win32') {
+    console.log('Go tests not supported on Windows - skipping')
+    process.exit(1)
+}
+
 // Skip Go tests if Go is not installed
 try {
     const proc = Bun.spawnSync(['go', 'version'])
